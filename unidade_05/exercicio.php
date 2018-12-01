f5046762268943594ee329d4f4a51d67897f8e90
victor_hugo_valle <vhavhugo@gmail.com>
2018-12-01 17:10:08 -0200

    Mostrar dados em uma lista

--- unidade_05/exercicio.php
+++ unidade_05/exercicio.php
@@ -10,9 +10,16 @@
         <script src="jquery.js"></script>
         <script>
             $.getJSON('_json/produtos.json', function(data){
+                var elemento;
+
+                elemento = "<ul>";
                 $.each(data, function(i,valor){
-                    console.log(valor.nomeproduto);
-                })
+                    elemento += "<li>" + valor.nomeproduto + "</li>";
+                    elemento += "<li>" + valor.precounitario + "</li>";
+                });
+                elemento += "</ul>";
+
+                $('div#listagem').html(elemento);
             });
         </script>
     </body>
