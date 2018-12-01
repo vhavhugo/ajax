2c4d78ce27f9177e548172b650cdec5f2b06de16
victor_hugo_valle <vhavhugo@gmail.com>
2018-12-01 16:51:12 -0200

    Chamar carregamento através de um evento

--- unidade_04/exercicio.php
+++ unidade_04/exercicio.php
@@ -7,29 +7,38 @@
     </head>
 
     <body>
+        <button onclick="carregarDados()" id="botao">Carregar</button>
         <div id="listagem"></div>
         
         <script src="jquery.js"></script>
         <script>
-            $.ajax({
-                url:'_xml/produtos.xml'
-            }).then(sucesso, falha);
+            $('botton#botao').click(function () {
+                $('div#listagem').css('display','block');
+                carregarDados();
+            });
 
-            function sucesso(arquivo){
-               var elemento;
-               elemento = "<ul>";
-                $(arquivo).find('produto').each(function(){
-                    var nome = $(this).find('nomeproduto').text();
-                    var preco = $(this).find('precounitario').text();
-                    elemento += "<li class='nome'>" + nome + "</li>";
-                    elemento += "<li class='preco'>" + preco + "</li>";
-                });
-               elemento += "</ul>";
-               $('div#listagem').html(elemento);
-            }
+            function carregarDados() {
 
-            function falha() {
+                $.ajax({
+                    url: '_xml/produtos.xml'
+                }).then(sucesso, falha);
 
+                function sucesso(arquivo) {
+                    var elemento;
+                    elemento = "<ul>";
+                    $(arquivo).find('produto').each(function () {
+                        var nome = $(this).find('nomeproduto').text();
+                        var preco = $(this).find('precounitario').text();
+                        elemento += "<li class='nome'>" + nome + "</li>";
+                        elemento += "<li class='preco'>" + preco + "</li>";
+                    });
+                    elemento += "</ul>";
+                    $('div#listagem').html(elemento);
+                }
+
+                function falha() {
+
+                }
             }
         </script>
     </body>
