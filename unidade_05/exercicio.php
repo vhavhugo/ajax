502cb22c0ef4dda41a3acb204f84135a7aca3012
victor_hugo_valle <vhavhugo@gmail.com>
2018-12-01 17:24:00 -0200

    Aplicar estilo pra carregar evento no Json

--- unidade_05/exercicio.php
+++ unidade_05/exercicio.php
@@ -3,24 +3,32 @@
     <head>
         <meta charset="UTF-8">
         <title>PHP com AJAX</title>
+        <link href="_css/estilo.css" rel="stylesheet">
     </head>
 
     <body>
+    <button id="botao">Carregar JSON</button>
         <div id="listagem"></div>
         <script src="jquery.js"></script>
         <script>
-            $.getJSON('_json/produtos.json', function(data){
-                var elemento;
-
-                elemento = "<ul>";
-                $.each(data, function(i,valor){
-                    elemento += "<li>" + valor.nomeproduto + "</li>";
-                    elemento += "<li>" + valor.precounitario + "</li>";
-                });
-                elemento += "</ul>";
-
-                $('div#listagem').html(elemento);
+            $('button#botao').click(function(){
+                $('div#listagem').css('display','block');
+                carregarDados();
             });
+            function carregarDados() {
+                $.getJSON('_json/produtos.json', function (data) {
+                    var elemento;
+
+                    elemento = "<ul>";
+                    $.each(data, function (i, valor) {
+                        elemento += "<li class='nome'>" + valor.nomeproduto + "</li>";
+                        elemento += "<li class='preco'>" + valor.precounitario + "</li>";
+                    });
+                    elemento += "</ul>";
+
+                    $('div#listagem').html(elemento);
+                });
+            }
         </script>
     </body>
 </html>
