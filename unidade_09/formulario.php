2c062321a63041cddd90ae311d941b60bd81bcb9
victor_hugo_valle <vhavhugo@gmail.com>
2018-12-01 22:41:03 -0200

    Alterando post no formulario

--- unidade_09/formulario.php
+++ unidade_09/formulario.php
@@ -1,6 +1,6 @@
 <?php
     // Criar objeto de conexao
-    $conecta = mysqli_connect("localhost","root","","andes");
+    $conecta = mysqli_connect("localhost","root","root","andes");
     if ( mysqli_connect_errno()  ) {
         die("Conexao falhou: " . mysqli_connect_errno());
     }
@@ -96,7 +96,25 @@
             });
             
             function alterarFormulario(dados) {
-
+                $.ajax({
+                   type:"POST",
+                   data: dados.serialize(),
+                   url:"alterar_transportadora.php",
+                   async:false
+                    
+                }).done(function(data){
+                    $sucesso = $.parseJSON(data)["sucesso"];
+                    $mensagem = $.parseJSON(data)["mensagem"];
+                    if($sucesso){
+                        $('#mensagem p').html($mensagem);
+                    }else{
+                        $('#mensagem p').html($mensagem);
+                    }
+                }).fail(function(){
+                        $('#mensagem p').html("Erro no sistema, tente mais tarde");
+                }).always(function(){
+                        $('#mensagem').show();
+                });
             }
         </script>
     </body>
